357189dd68837207ea73dfc95ba3cf8d9ff6542e
Brendon Calazans <brendoncalazans@outlook.com>
2022-08-15 21:52:19 -0300

    Finalizando implementação de Visão Computacional e Reconhecimento Facial

---
+++ Notebooks/Data_Science_Rastreamento_de_Objetos_Visao_Computacional.py
@@ -0,0 +1,35 @@
+import cv2
+from matplotlib.image import BboxImage
+
+print(cv2.__version__)
+
+rastreador = cv2.TrackerCSRT_create()
+
+video = cv2.VideoCapture(r'C:\Users\brcalazans\Desktop\Pessoal\Machine Learning e Data Science\Bases de dados\rua.mp4')
+ok, frame = video.read()
+
+bbox = cv2.selectROI('',frame)
+#print(bbox)
+#print(frame)
+
+ok = rastreador.init(frame, bbox)
+#print(ok)
+
+while True:
+    ok, frame = video.read()
+    if not ok:
+        break
+
+    ok, bbox = rastreador.update(frame)
+    print(bbox)
+
+    if ok:
+        (x, y, w, h) = [int(v) for v in bbox]
+        cv2.rectangle(frame, (x,y), (x + w, y+h), (0,255,0), 2, 1)
+    else:
+        cv2.putText(frame, 'Falha no rastreamento', (100,80), cv2.FONT_HERSHEY_SIMPLEX, .75, (0,0,255), 2)
+
+
+    cv2.imshow('rastreando', frame)
+    if cv2.waitKey(1) & 0XFF == 27:
+        break
